3da062d3551470859a1770db28f582292d692da5
moon <mseongwon@naver.com>
2023-08-17 20:55:11 +0900

    디자인 수정 2차

--- app/src/main/java/com/example/participant/Home2Activity.java
+++ app/src/main/java/com/example/participant/Home2Activity.java
@@ -2,13 +2,28 @@ package com.example.participant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home2Activity extends AppCompatActivity {
+
+    private Button home2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home2);
+
+        home2 = findViewById(R.id.home2);
+        home2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // seven Activity로 전환
+                Intent intent = new Intent(Home2Activity.this, SevenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/participant/TwoActivity.java
+++ app/src/main/java/com/example/participant/TwoActivity.java
@@ -8,20 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class TwoActivity extends AppCompatActivity {
-    private Button btn10_1; // 가능
-    private Button btn10_2; // 불가능
-    private Button btn10_3; // 다음
+    private Button btn2_1; // 가능
+    private Button btn2_2; // 불가능
+    private Button btn2_3; // 다음
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.two);
 
-        btn10_1=findViewById(R.id.btn10_1);
-        btn10_2=findViewById(R.id.btn10_2);
-        btn10_3=findViewById(R.id.btn10_3);
+        btn2_1=findViewById(R.id.btn2_1);
+        btn2_2=findViewById(R.id.btn2_2);
+        btn2_3=findViewById(R.id.btn2_3);
 
-        btn10_3.setOnClickListener(new View.OnClickListener() {
+        btn2_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
